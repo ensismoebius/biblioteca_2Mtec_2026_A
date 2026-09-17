c3fb50aa0fddcd24e7b551257e40d51282e5b2bb
ensismoebius <ensismoebius@gmail.com>
2026-09-17 11:47:58 -0300

    teste: arquivo PHP ruim (deve reprovar no code-intelligence)

---
+++ app/Models/x.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+class x
+{
+    public function a($b, $c) {
+        if ($b > 1) {
+            if ($c > 1) {
+                return $b + $c;
+            }
+        }
+        return $b;
+    }
+}
