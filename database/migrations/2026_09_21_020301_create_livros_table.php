2ddfc124b56bfe98b2038c260ee665ea176e6610
caralyne <mudestocarolyne@gmail.com>
2026-09-25 03:44:15 -0300

    Update 2026_09_21_020301_create_livros_table.php

--- database/migrations/2026_09_21_020301_create_livros_table.php
+++ database/migrations/2026_09_21_020301_create_livros_table.php
@@ -12,27 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('LIVROS', function (Blueprint $table) {
-            // Chave Primária Auto-Incremento
             $table->id('LVRCODIGO');
-
-            // Título: VARCHAR(150), NOT NULL e com Índice para busca rápida
             $table->string('LVRTITULO', 150)->index();
-
-            // ISBN: VARCHAR(15), Nulo permitido, Único quando preenchido
             $table->string('LVRISBN', 15)->nullable()->unique();
-
-            // Edição: Inteiro, Nulo permitido
             $table->integer('LVREDICAO')->nullable();
-
-            // Data de Publicação: DATE, Nulo permitido
             $table->date('LVRDTPUBLIC')->nullable();
-
-            // Sinopse: TEXT, Nulo permitido
             $table->text('LVRSINOPSE')->nullable();
-
-            // Idade Mínima: Inteiro, Nulo permitido
             $table->integer('LVRFAIXAETARIA')->nullable();
-
             $table->timestamps();
         });
     }
